927366321ac303147ee897c2eee4abd791da80f6
ivan.myzherytskyi <mizhek1@gmail.com>
2019-09-05 13:24:11 +0300

    rename settings activity

--- app/src/main/java/com/example/nytimes/activities/SettingsActivity.java
+++ app/src/main/java/com/example/nytimes/activities/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.example.nytimes.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.nytimes.R;
@@ -13,7 +12,7 @@ public class SettingsActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.activity_settings);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
