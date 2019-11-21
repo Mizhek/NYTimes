6ec6fd7dcb2d05a04462b2bf7f2a2ced065f6a94
ivan.myzherytskyi <mizhek1@gmail.com>
2019-11-21 11:56:52 +0200

    Move to new api key and hide it

--- app/src/main/java/com/example/nytimes/interfaces/MostPopularApi.java
+++ app/src/main/java/com/example/nytimes/interfaces/MostPopularApi.java
@@ -1,5 +1,6 @@
 package com.example.nytimes.interfaces;
 
+import com.example.nytimes.BuildConfig;
 import com.example.nytimes.data.NYTimesApi;
 
 import retrofit2.Call;
@@ -7,6 +8,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface MostPopularApi {
-    @GET("/svc/mostpopular/v2/{mostPopularType}/{period}.json?api-key=X258G8t8AYoGZQMOuneVDsCq3OBFn7Nv")
+    @GET("/svc/mostpopular/v2/{mostPopularType}/{period}.json?api-key=" + BuildConfig.API_KEY)
     Call<NYTimesApi> getArticles(@Path("mostPopularType") String mostPopularType, @Path("period") int period);
 }
